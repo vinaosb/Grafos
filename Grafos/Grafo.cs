386c7dd51794b8687e260a9a55d2502ec39466e7
Vinícius Schwinden Berkenbrock <vsbb13@hotmail.com>
2018-09-23 04:00:57 -0300

    Conf

---
+++ Grafos/Grafo.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grafos
+{
+    class Grafo
+    {
+        public class Vertice
+        {
+            public int vid { get; set; }
+            private SortedDictionary<int, int> proximos = new SortedDictionary<int, int>(); // v2, custo
+            private SortedDictionary<int, int> anteriores = new SortedDictionary<int, int>();
+
+            public Vertice(int vid = 0)
+            {
+                this.vid = vid;
+            }
+
+            public void addProximo(int v2, int custo = 0)
+            {
+                proximos.Add(v2, custo);
+            }
+
+            public void addAnterior(int v1, int custo = 0)
+            {
+                anteriores.Add(v1, custo);
+            }
+
+            public void removeAnt (int id)
+            {
+                if (anteriores.ContainsKey(id))
+                    anteriores.Remove(id);
+            }
+
+            public void removeProx(int id)
+            {
+                if (proximos.ContainsKey(id))
+                    proximos.Remove(id);
+            }
+
+            public void removeAdj (int id)
+            {
+                removeAnt(id);
+                removeProx(id);
+            }
+
+            public SortedDictionary<int, int> GetAdjacentes()
+            {
+                var temp = new SortedDictionary<int, int>();
+                foreach (var v in proximos)
+                {
+                    temp.TryAdd(v.Key, v.Value);
+                }
+                foreach (var v in anteriores)
+                {
+                    temp.TryAdd(v.Key, v.Value);
+                }
+                return temp;
+            }
+
+            public SortedDictionary<int,int> GetProximos()
+            {
+                return proximos;
+            }
+        }
+
+        static Random r = new Random();
+        private SortedDictionary<int, Vertice> Verticess;
+
+        public Grafo()
+        {
+            Verticess = new SortedDictionary<int, Vertice>();
+            Verticess.Clear();
+        }
+
+        public void AddVertice(int id)
+        {
+            Vertice v = new Vertice(id);
+            Verticess.Add(id, v);
+        }
+
+        public void RemoveVertice(int id)
+        {
+            if (Verticess.ContainsKey(id))
+            {
+                foreach (var v in Verticess.GetValueOrDefault(id).GetAdjacentes())
+                {
+                    Desconecta(v.Key, id);
+                }
+                Verticess.Remove(id);
+            }
+        }
+
+        public void Conecta(int id1, int id2, int c = 0)
+        {
+            ConectaOrdenado(id1, id2, c);
+            ConectaOrdenado(id2, id1, c);
+        }
+
+        public void ConectaOrdenado(int id1, int id2, int c = 0)
+        {
+            if (Verticess.ContainsKey(id2) && Verticess.ContainsKey(id1))
+            {
+                Verticess.GetValueOrDefault(id1).addProximo(id2, c);
+                Verticess.GetValueOrDefault(id2).addAnterior(id1, c);
+            }
+        }
+
+        public void Desconecta(int id1, int id2)
+        {
+            DesconectaOrdenado(id1, id2);
+            DesconectaOrdenado(id2, id1);
+        }
+
+        public void DesconectaOrdenado(int id1, int id2)
+        {
+            if (Verticess.ContainsKey(id1) && Verticess.ContainsKey(id2))
+            {
+                if (Verticess.GetValueOrDefault(id1).GetAdjacentes().ContainsKey(id2))
+                {
+                    Verticess.GetValueOrDefault(id1).removeAdj(id2);
+                }
+            }
+        }
+
+        public int Ordem ()
+        {
+            return Verticess.Count;
+        }
+
+        public SortedDictionary<int,Vertice> Vertices()
+        {
+            return Verticess;
+        }
+
+        public Vertice umVertice()
+        {
+            return Verticess.GetValueOrDefault(GetRandomKey());
+        }
+
+        private int GetRandomKey ()
+        {
+            int[] keys = new int[Verticess.Count];
+            Verticess.Keys.CopyTo(array: keys, index: 0);
+            return keys[r.Next(keys.Length)];
+        }
+
+        public SortedDictionary<int, int> Adjacentes(int vid)
+        {
+            if (!Verticess.ContainsKey(vid))
+                return new SortedDictionary<int, int>();
+            return Verticess.GetValueOrDefault(vid).GetAdjacentes();
+        }
+
+        public int Grau (int vid)
+        {
+            return Adjacentes(vid).Count;
+        }
+
+        public bool EhRegular()
+        {
+            int test = Grau(umVertice().vid);
+            foreach (var v in Verticess)
+            {
+                if (Grau(v.Key) != test)
+                    return false;
+            }
+            return true;
+        }
+
+        public bool EhCompleto()
+        {
+            foreach (var v in Verticess)
+            {
+                if (Grau(v.Key) != Verticess.Count)
+                    return false;
+            }
+            return true;
+        }
+
+        public SortedSet<int> FechoTransitivo(int vid)
+        {
+            var Conj = new SortedSet<int>();
+            ProcuraFechoTransitivo(vid, Conj);
+            return Conj;
+        }
+
+        private SortedSet<int> ProcuraFechoTransitivo(int vid, SortedSet<int> Conj)
+        {
+            Conj.Add(vid);
+            foreach (var v in Verticess.GetValueOrDefault(vid).GetAdjacentes())
+            {
+                if (!Conj.Contains(v.Key))
+                    Conj.UnionWith(ProcuraFechoTransitivo(v.Key, Conj));
+            }
+            return Conj;
+        }
+
+        public bool EhConexo()
+        {
+            var Conj = new SortedSet<int>();
+            foreach (var v in Verticess)
+            {
+                Conj.Add(v.Key);
+            }
+
+            return Conj.SetEquals(FechoTransitivo(umVertice().vid));
+        }
+
+        public bool EhArvore()
+        {
+            var vis = new HashSet<int>();
+            return EhConexo() && !HaCiclo(umVertice().vid, umVertice().vid,vis);
+        }
+
+        private bool HaCiclo(int id, int idAnt, HashSet<int> visitados)
+        {
+            if (visitados.Contains(id))
+                return true;
+            visitados.Add(id);
+            foreach (var v in Verticess.GetValueOrDefault(id).GetAdjacentes())
+            {
+                if (!v.Key.Equals(idAnt))
+                    if (HaCiclo(v.Key, id, visitados))
+                        return true;
+            }
+            visitados.Remove(id);
+            return false;
+        }
+        
+        public List<int> OrdenacaoTopologica ()
+        {
+            List<int> topo = new List<int>();
+            Dictionary<int,bool> mark = new Dictionary<int,bool>();
+            int temp = GetRandomKey();
+
+            while (Verticess.Count != mark.Count)
+            {
+                while (mark.ContainsKey(temp))
+                    temp = GetRandomKey();
+                VisitaTopologica(temp, topo, mark);
+            }
+            topo.Reverse();
+            return topo;
+        }
+
+        private void VisitaTopologica (int n, List<int> topo, Dictionary<int,bool> mark)
+        {
+            if (mark.ContainsKey(n) && mark.GetValueOrDefault(n) == true)
+                return;
+            if (mark.ContainsKey(n) && mark.GetValueOrDefault(n) == false)
+            {
+                System.Console.WriteLine("Erro, Grafo Nao é DAG");
+                System.Console.ReadKey();
+                Environment.Exit(10);
+            }
+            mark.Add(n, false);
+            foreach (var v in Verticess.GetValueOrDefault(n).GetProximos())
+            {
+                VisitaTopologica(v.Key, topo, mark);
+            }
+            mark.Remove(n);
+            mark.Add(n, true);
+            topo.Add(n);
+        }
+    }
+}
--- Grafos/Program.cs
+++ Grafos/Program.cs
@@ -1,12 +1,148 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Grafos
 {
     class Program
     {
+        public static Grafo G { get; set; }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            G = new Grafo();
+
+            G.AddVertice(5401);
+            G.AddVertice(5402);
+            G.AddVertice(5403);
+            G.AddVertice(5404);
+            G.AddVertice(5405);
+            G.AddVertice(5406);
+            G.AddVertice(5407);
+            G.AddVertice(5408);
+            G.AddVertice(5409);
+            G.AddVertice(5410);
+            G.AddVertice(5411);
+            G.AddVertice(5412);
+            G.AddVertice(5413);
+            G.AddVertice(5414);
+            G.AddVertice(5415);
+            G.AddVertice(5416);
+            G.AddVertice(5417);
+            G.AddVertice(5418);
+            G.AddVertice(5419);
+            G.AddVertice(5420);
+            G.AddVertice(5421);
+            G.AddVertice(5422);
+            G.AddVertice(5423);
+            G.AddVertice(5424);
+            G.AddVertice(5425);
+            G.AddVertice(5426);
+            G.AddVertice(5427);
+            G.AddVertice(5428);
+            G.AddVertice(5429);
+            G.AddVertice(5430);
+            G.AddVertice(5431);
+            G.AddVertice(5432);
+            G.AddVertice(5433);
+            G.AddVertice(5434);
+            G.AddVertice(5453);
+
+            G.AddVertice(5105);
+            G.AddVertice(5161);
+            G.AddVertice(7174);
+            G.AddVertice(5512);
+            G.AddVertice(5245);
+
+            //Vermelho
+            G.ConectaOrdenado(5402, 5404);
+            G.ConectaOrdenado(5404, 5408);
+            G.ConectaOrdenado(5404, 5410);
+            G.ConectaOrdenado(5404, 5414);
+            G.ConectaOrdenado(5408, 5417);
+            G.ConectaOrdenado(5408, 5420);
+            G.ConectaOrdenado(5408, 5423);
+            G.ConectaOrdenado(5408, 5413);
+            G.ConectaOrdenado(5408, 5415);
+            G.ConectaOrdenado(5408, 5416);
+            G.ConectaOrdenado(5410, 5412);
+            G.ConectaOrdenado(5417, 5419);
+            G.ConectaOrdenado(5417, 5427);
+            G.ConectaOrdenado(5417, 5453);
+            G.ConectaOrdenado(5427, 5433);
+            G.ConectaOrdenado(5453, 5433);
+
+            //Amarelo Claro
+            G.ConectaOrdenado(5161, 7174);
+            G.ConectaOrdenado(5161, 5405);
+            G.ConectaOrdenado(7174, 5409);
+            G.ConectaOrdenado(7174, 5420);
+            G.ConectaOrdenado(5405, 5425);
+            G.ConectaOrdenado(5405, 5430);
+            G.ConectaOrdenado(5512, 5245);
+            G.ConectaOrdenado(5512, 5409);
+            G.ConectaOrdenado(5245, 5420);
+
+            //Verde
+            G.ConectaOrdenado(5403, 5429);
+            G.ConectaOrdenado(5403, 5413);
+            G.ConectaOrdenado(5403, 5415);
+            G.ConectaOrdenado(5415, 5421);
+            G.ConectaOrdenado(5421, 5426);
+            G.ConectaOrdenado(5416, 5430);
+
+            //Azul
+            G.ConectaOrdenado(5105, 5406);
+            G.ConectaOrdenado(5406, 5411);
+            G.ConectaOrdenado(5411, 5412);
+            G.ConectaOrdenado(5412, 5418);
+            G.ConectaOrdenado(5412, 5424);
+
+            //Branco
+            G.ConectaOrdenado(5407, 5428);
+
+            //Laranja
+            G.ConectaOrdenado(5414, 5422);
+            G.ConectaOrdenado(5414, 5418);
+            G.ConectaOrdenado(5414, 5431);
+            G.ConectaOrdenado(5414, 5429);
+
+            //Roxo
+            G.ConectaOrdenado(5423, 5432);
+
+
+
+            Console.WriteLine(G.EhArvore());
+            Console.WriteLine(G.EhConexo());
+            var list = G.OrdenacaoTopologica();
+            string temp = "";
+            foreach (var l in list)
+            {
+                switch (l)
+                {
+                    case 5105:
+                        temp = "EEL";
+                        break;
+                    case 5161:
+                        temp = "MTM";
+                        break;
+                    case 7174:
+                        temp = "MTM";
+                        break;
+                    case 5245:
+                        temp = "MTM";
+                        break;
+                    case 5512:
+                        temp = "MTM";
+                        break;
+                    default:
+                        temp = "INE";
+                        break;
+                }
+                Console.WriteLine(temp + l);
+            }
+            Console.ReadKey();
         }
     }
 }
